09d2b9ba8c6bbcaed98ac9e131ac8804b0cf7e8e
Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-08-03 12:50:48 +0100

    Fetching single product

--- models/productModel.js
+++ models/productModel.js
@@ -11,7 +11,21 @@ module.exports = class Product {
     this.price = price;
   }
 
-  save() {}
+  async save() {
+    try {
+      const client = await db.connect();
+      const result = await client.query(
+        "INSERT INTO products (title, price, imageurl, description) VALUES ($1, $2, $3, $4)",
+        [this.title, this.price, this.imageUrl, this.description]
+      );
+      client.release(); // Release the client back to the pool
+      console.log(result);
+      return result.rows;
+    } catch (error) {
+      console.error("Error executing query:", error);
+      throw error;
+    }
+  }
 
   static deleteById(id) {}
 
@@ -27,5 +41,18 @@ module.exports = class Product {
     }
   }
 
-  static findById(id) {}
+  static async findById(id) {
+    try {
+      const client = await db.connect();
+      const result = await client.query(
+        "SELECT * FROM products WHERE products.id = $",
+        [id]
+      );
+      // client.release(); // Release the client back to the pool
+      return result.rows;
+    } catch (error) {
+      console.error("Error executing query:", error);
+      throw error;
+    }
+  }
 };
